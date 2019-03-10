b39d768b0ec201ae37c0afcc424447ffc8a8c115
Pithani.Kishan Dev <44198442+kishandev1999@users.noreply.github.com>
2019-03-10 19:10:32 +0530

    initial commit

---
+++ areaofcircle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	float area,r;
+	printf("enter the area of r");
+	scanf("%f",&r);
+	area = 3.14*(r*r);
+	printf("the area is %f",area);
+	getch();
+}
+
